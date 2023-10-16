b185a546ce67e5ca92e4c3823c3633140494306b
Aayush <just.another.3d.object@gmail.com>
2023-10-16 23:19:59 +0530

    Target encoding

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder 
+from sklearn import metrics
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+from pickle import dump, load
 
 df = pd.read_csv('url_data.csv')
+
+lb_make = LabelEncoder()
+df["type_code"] = lb_make.fit_transform(df["type"])
+df["type_code"].value_counts()
+
 X = df['url'].values
-y = df['type'].values
+vectorizer = TfidfVectorizer()
+vectorizer.fit(X)
+X = vectorizer.transform(X)
+dump(vectorizer, open('vectorizer.pkl', "wb"))
+
+y = df['type_code']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, test_size=0.2,shuffle=True, random_state=8)
